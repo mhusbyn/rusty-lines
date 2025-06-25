a936aa5c927a00725f0bdf1ac4b2b9706701d70a
Martin Husbyn <mhusbyn@gmail.com>
2025-06-25 22:09:44 +0100

    fix: move test files to subfolder
    The test file was still being compiled, causing the CI pipeline to fail.
    Move it into a subfolder so that the compiler doesn't see it.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,6 @@
 #[test]
 fn test_it_returns_line_count() {
-    let test_file = std::path::Path::new("tests/count_me.rs");
+    let test_file = std::path::Path::new("tests/testdata/count_me.rs");
     assert_eq!(
         rusty_lines::run(test_file),
         format!("{}\t5", test_file.display())
@@ -9,7 +9,7 @@ fn test_it_returns_line_count() {
 
 #[test]
 fn test_it_errors_for_wrong_file_extension() {
-    let test_file = std::path::Path::new("tests/test_text_file.txt");
+    let test_file = std::path::Path::new("tests/testdata/test_text_file.txt");
     assert_eq!(
         rusty_lines::run(test_file),
         format!("'{}' is not a Rust file!", test_file.display())
